e6932b006f52ecdf34085fdac966e262a1f1648f
Shevaister <degrae000@gmail.com>
2021-06-20 16:40:23 +0300

    1.6

--- main.go
+++ main.go
@@ -1,32 +1,81 @@
 package main
 
 import(
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"io/ioutil"
-	"strconv"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-func flux(a string) {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts/" + a) 
+func Parser() {
+	data := make([]map[string]interface{}, 0)
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts?userId=7") 
 	if err != nil {
-		fmt.Println("read1",err)
-		return
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
-	
-	n, err := ioutil.ReadAll(resp.Body)
+
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("read2", err)
-		return
+		fmt.Println(err)
 	}
-	ioutil.WriteFile("results/" + a + ".txt", n, 0777) 
+	
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+	db, err := sql.Open("mysql", "mysql:mysql@tcp(127.0.0.1:3306)/parse")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	for _, value := range data {
+		go flux1stGrade(db, value)
+	}
+	
+	var input string
+	fmt.Scanln(&input)
+}
+
+func flux1stGrade(db *sql.DB, data map[string]interface{}) {
+	_, err := db.Exec("INSERT INTO posts VALUES(?, ?, ?, ?)", data["userId"], data["id"], data["title"], data["body"])
+	if err != nil {
+		fmt.Println(err)
+	}
+	dataComments := make([]map[string]interface{}, 0)
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/comments?postId=" + fmt.Sprintf("%v", data["id"]))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = json.Unmarshal(body, &dataComments)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, value := range dataComments {
+		go flux2ndGrade(db, value)
+	}
+
+}
+
+func flux2ndGrade(db *sql.DB, data map[string]interface{}) {
+	_, err := db.Exec("INSERT INTO comments VALUES(?, ?, ?, ?, ?)", data["postId"], data["id"], data["name"], data["email"], data["body"])
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
-	for i := 1; i <= 100; i++ {
-		go flux(strconv.Itoa(i))
-	}
-	var input string
-	fmt.Scanln(&input)
+	Parser()
 }
